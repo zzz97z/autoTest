cff737f2bba5c799de5540f7ddd9d7324756e276
mike <1747248477@qq.com>
2021-09-08 11:15:15 +0800

    优化用例模板，向excel插入测试结果

--- the_xxx_project/xxx_module/test_ddt_unittest_demo.py
+++ the_xxx_project/xxx_module/test_ddt_unittest_demo.py
@@ -4,24 +4,24 @@
 from ddt import unpack,data,ddt
 import unittest
 from public_method.requests_json import *
-from public_method.excel_read import get_xl
+from public_method.ex_operation import xl_operation
 import json
 @ddt
 class Test(unittest.TestCase):
     # 选择文件读取
-    test_data = get_xl("D:\pythonScript\interface_auto_test\data\demo.xls","Sheet1")
+    test_data = xl_operation().xls_get("D:\pythonScript\interface_auto_test\data\demo.xls","Sheet1")
     @data(*test_data)
     @unpack
-    def test_a(self,url,method,header,body):
+    def test_a(self,caseId,url,method,header,body):
         lg=request_port(url,method,json.loads(header),json.loads(body)).request_post()
         self.mg=lg.json()['status']
         param=lg.json()['params']
         if self.mg==200:
             res='PASS'
-            print(res)
+            xl_operation().xls_upadate("D:\pythonScript\interface_auto_test\data\demo.xls",caseId,6,res)
         else:
             res='FAIL'
-            print('')
+            xl_operation().xls_upadate("D:\pythonScript\interface_auto_test\data\demo.xls", caseId, 6, res)
             self.assertEquals(self.mg,200,msg='\n{}\n{}'.format(res,param,self.mg))
 
 if __name__ == '__main__':
