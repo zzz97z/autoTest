0a5b4c85aa4e6c6b7ebe34936463ed658cad4e95
mike <1747248477@qq.com>
2021-09-08 11:59:34 +0800

    修改

--- public_method/sql_conn_operation.py
+++ public_method/sql_conn_operation.py
@@ -1,58 +1,45 @@
-#作者：zengziwei
-#创建时间：2021/8/13 10:56
-#文件名：sql_conn_operation.py
+# 作者：zengziwei
+# 创建时间：2021/8/13 10:56
+# 文件名：sql_conn_operation.py
+
 import pymysql
-class Mysql():
-    def __init__(self,host,user,paw,db,port):
-        """链接数据库的基本信息"""
-        self.host=host
-        self.user=user
-        self.paw=paw
-        self.db_name=db
-        self.port=port
-        self.db = pymysql.connect(host=self.host,user=self.user,password=self.paw,db=self.db_name,port=self.port)
+class Mysql:
+    def __init__(self, db_link, db_user, db_password, db_name, db_port):
+        self.db_link = db_link
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_name = db_name
+        self.db_port = db_port
+        self.db_conn = pymysql.connect(host=self.db_link, user=self.db_user, password=self.db_password,
+                                       db=self.db_name, port=self.db_port)
 
-    def find_all(self,sql):
-        """返回当前sql语句查询到的所有数据"""
-        # 使用 cursor() 方法创建一个游标对象 cursor
-        cursor = self.db.cursor()
-        # 使用 execute()  方法执行 SQL 查询
+    def find_all(self, sql):
+        cursor = self.db_conn.cursor()
         cursor.execute(sql)
-        # 使用 fetchone() 方法获所有数据.
         data = cursor.fetchall()
-        # 关闭数据库连接
         cursor.close()
-        self.db.close()
+        self.db_conn.close()
         return data
 
-    def find_one(self,sql):
-        """返回当前sql语句查询到的单行数据"""
-        # 使用 cursor() 方法创建一个游标对象 cursor
-        cursor = self.db.cursor()
-        # 使用 execute()  方法执行 SQL 查询
+    def find_one(self, sql):
+        cursor = self.db_conn.cursor()
         cursor.execute(sql)
-        # 使用 fetchone() 方法获所有数据.
         data = cursor.fetchone()
-        # 关闭数据库连接
-        self.db.close()
+        cursor.close()
+        self.db_conn.close()
         return data
 
-    def execute_sql(self,sql):
-        """支持增、删、改sql语句"""
-        # 使用 cursor() 方法创建一个游标对象 cursor
-        cursor = self.db.cursor()
+    def del_add_up_sql(self, sql):
+        cursor = self.db_conn.cursor()
         try:
-            # 执行SQL语句
             cursor.execute(sql)
-            # 提交修改
-            self.db.commit()
-            return True
+            self.db_conn.commit()
         except Exception as msg:
-            # 发生错误时回滚
-            self.db.rollback()
-            print("执行失败：%s"%msg)
+            self.db_conn.rollback()
+            print(msg)
 
-
-
-if __name__=="__main__":
-    Mysql()
+if __name__ == "__main__":
+    conn = Mysql('db_link', 'db_user', 'db_password', 'db_name', 3306)
+    sql = 'select * from m_user'
+    res = conn.find_all(sql)
+    print(res)
